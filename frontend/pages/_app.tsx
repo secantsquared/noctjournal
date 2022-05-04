3d5c10d9de0c63c73cb165edf90d89e43cdc384d
Ryan Boris <19439753+secantsquared@users.noreply.github.com>
2022-05-04 04:34:10 -0400

    style(deps): add tailwind css deps

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
-import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
